1091c047b5d9ff4fba3a7d405e4f80de6b3de363
Finn <Grewoss@gmail.com>
2018-05-22 01:29:10 +0200

    Implemented basics for the neo4j db

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ from src.database import Database
 bot = commands.Bot(command_prefix='~')
 bot.remove_command('help')
 
-database = Database(MONGO_DB_KEY)
+database = Database("grewoss")
 
 embedgenerator = None
 
 
 @bot.event
 async def on_ready():
-    print("Ready")
     global embedgenerator
     embedgenerator = EmbedGenerator(bot)
+    for guild in bot.guilds:
+        for role in guild.roles:
+            await database.create_role(role.name, role.id)
+        for user in guild.members:
+            await database.create_user(user)
+    print("done")
+
 
 @bot.command(aliases=["?", "hilfe"])
 async def help(ctx, *args):
@@ -28,6 +34,6 @@ async def github(ctx):
 
 @bot.command()
 async def xp(ctx):
-    await ctx.send("Du hast: {}".format(await database.get_user_xp(ctx.message.author.id)))
+    await ctx.send(await database.find_user(ctx.message.author.id))
 
 bot.run(BOT_TOKEN)
--- src/database.py
+++ src/database.py
@@ -1,61 +1,99 @@
-import pymongo
-from src.secrets import MONGO_DB_KEY
+from py2neo import Node, Graph, Relationship
+from py2neo.ogm import GraphObject, Property, RelatedFrom, RelatedTo
+import discord
+
+
+class Role(GraphObject):
+    __primarykey__ = "id"
+
+    id = Property()
+    name = Property()
+
+    users = RelatedFrom("User", "HAS")
+
+
+class User(GraphObject):
+    __primarykey__ = "id"
+
+    id = Property()
+    name = Property()
+    levels = Property()
+    xp = Property()
+    helper_votes = Property()
+    last_vote_made_on = Property()
+
+    roles = RelatedTo("Role", "HAS")
+
 
 class Database(object):
-    def __init__(self, mongo_db_key):
-        self._client = pymongo.MongoClient(mongo_db_key)
-        self._user_base = self._client.discord_db.users
+    def __init__(self, password: str):
+        self.graph = Graph("bolt://localhost:7687", password=password)
+        print(self.graph)
     
-    async def create_new_user(self, user_id: int, user_name):
-        try:
-            post = {
-                "_id": user_id,
-                "user_name": user_name,
-                "xp": 0,
-                "gamble_won": 0,
-                "gamble_lost": 0,
-                "myrole": None,
-                "helper_votes": 0,
-                "last_vote": None
-            }
-            post_id = self._user_base.insert_one(post).inserted_id
-            return post_id
-        except:
-            return None
+
+    async def create_user(self, discord_user: discord.Member):
+        if User.select(self.graph, discord_user.id).first() is None:
+            user = User()
+            user.id = discord_user.id
+            user.name = discord_user.name
+            user.levels = 0
+            user.xp = 0
+            user.helper_votes = 0
+            user.last_vote_made_on = None
+
+            for _role in discord_user.roles:
+                new_role = Role.select(self.graph, _role.id).first()
+                user.roles.add(new_role)
+            
+            self.graph.push(user)
     
-    async def find_user(self, user_id: int):
-        try:
-            print(self._user_base.full_name)
-            found = self._user_base.find_one({"_id": user_id})
-            print(found)
-            return found
-        except:
-            return None
+    async def create_role(self, role_name: str, role_id: int):
+        if Role.select(self.graph, role_id).first() is None:
+            role = Role()
+            role.id = role_id
+            role.name = role_name
+            
+            self.graph.push(role)
     
-    async def update_user(self, user_id:int, change_dict: dict):
-        try:
-            update = self._user_base.update_one(
-                {"_id": user_id}, 
-                {"$set": change_dict}
-                )
-            return update
-        except:
-            return None
+    async def find_user(self, _user_id: int):
+        user = User.select(self.graph, _user_id).first()
+        print(user.xp)
+        print(user.levels)
+        return user
     
-    async def get_all(self):
-        try:
-            data = self._user_base.find()
-            r_data = {}
-            for d in data:
-                r_data[d["_id"]] = d
-            return r_data
-        except:
-            return None
+    # async def connect_user_to_role(self, discord_id: int, role_id: int):
+    #     user_node = self.graph.find("User", "discord_id", discord_id)
+    #     role_node = self.graph.find("Role", "role_id", role_id)
+    #     relationship = Relationship(user_node, "HAS", role_node)
+    #     self.graph.create(relationship)
+
+# class Database(object):
+#     def __init__(self, password: str):
+#         self.graph = Graph("bolt://localhost:7687", password=password)
+#         print(self.graph)
     
-    async def get_user_xp(self, user_id: int):
-        try:
-            data = await self.find_user(user_id)
-            return data["xp"]
-        except Exception as e:
-            print(e)
-            return None
+
+#     async def create_user(self, discord_id: int, user_name: str):
+#         node = Node("User")
+#         node["discord_id"] = discord_id
+#         node["name"] = user_name
+#         node["level"] = 0
+#         node["xp"] = 0
+#         node["votes"] = 0
+
+#         self.graph.create(node)
+    
+#     async def create_role(self, role_name: str, role_id: int):
+#         node = Node("Role")
+#         node["name"] = role_name
+#         node["role_id"] = role_id
+        
+#         self.graph.create(node)
+    
+#     async def connect_user_to_role(self, discord_id: int, role_id: int):
+#         user_node = self.graph.find("User", "discord_id", discord_id)
+#         role_node = self.graph.find("Role", "role_id", role_id)
+#         relationship = Relationship(user_node, "HAS", role_node)
+#         self.graph.create(relationship)
+
+
